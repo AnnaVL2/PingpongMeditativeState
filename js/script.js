5de74a0d7ba9e388c4128f274127b559239aaf21
AnnaVL2 <43822073+AnnaVL2@users.noreply.github.com>
2021-04-13 20:56:13 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -119,6 +119,33 @@ function keyUpHandler(event){
 }
 /* moving paddles section end */
 
+function reset(){
+    // reset ball's value to older values
+    ball.x = canvas.width / 2;
+    ball.y = canvas.height / 2;
+    ball.speed = 7;
+
+    // changes the direction of ball
+    ball.velocityX = -ball.velocityX;
+    ball.velocityY = -ball.velocityY;
+}
+
+// collision Detect function
+function collisionDetect (player, ball){
+    // returns true or false
+    player.top = player.y;
+    player.right = player.x + player.width;
+    player.bottom = player.y + player.height;
+    player.left = player.x;
+
+    ball.top = ball.y - ball.radius;
+    ball.right = ball.x + ball.radius;
+    ball.bottom = ball.y + ball.radius;
+    ball.left = ball.x - ball.radius;
+
+    return ball.left < player.right && ball.top < player.bottom && ball.right > player.left && ball.bottom > player.top;
+}
+
 function update(){
     // move the paddle
     if (upArrowPressed && user.y > 0) {
@@ -128,6 +155,29 @@ function update(){
     }
 
     // check if ball hits top or bottom wall
+    if (ball.y + ball.radius >= canvas.height || ball.y - ball.radius <= 0){
+        // play wallHitSound
+
+        ball.velocityY = -ball.velocityY;
+    }
+
+    // if ball hit on right wall
+    if(ball.x + ball.radius >= canvas.width){
+        // play scoreSound
+
+        // then user scored 1 point
+        user.score += 1;
+        reset();
+    }
+
+    // if ball hit on left wall
+    if (ball.x - ball.radius <= 0){
+        // play scoreSound
+
+        // then ai scored 1 point
+        ai.score += 1;
+        reset();
+    }
 
     // move the ball
     ball.x += ball.velocityX;
@@ -136,7 +186,32 @@ function update(){
     // ai paddle movement
 
     // collision detection on paddles
+    
+    let player = (ball.x < canvas.width / 2) ? user : ai;
 
+    if (collisionDetect(player, ball)){
+        // play hitSound
+
+        // default angle is 0deg in Radian
+        let angle = 0;
+
+        // if ball hit the top of paddle
+        if (ball.y < (player.y + player.height / 2)){
+            // then -1 * Math.PI / 4 = -45deg
+            angle = -1 * Math.PI / 4;
+        } else if (ball.y > (player.y + player.height / 2)){
+            // if it hit the bottom of paddle
+            // then angle will be Math.PI / 4 = 45deg
+            angle = 1 * Math.PI / 4;
+        }
+
+        /* change velocity of ball according to on which paddle the ball hitted */
+        ball.velocityX = (player === user ? 1 : -1) * ball.speed * Math.cos(angle);
+        ball.velocityY = ball.speed * Math.sin(angle);
+
+        // increase ball speed
+        ball.speed += 0.2;
+    }
 }
 
 function render(){
